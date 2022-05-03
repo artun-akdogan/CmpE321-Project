5ff2497f624d2df01bcb905e306e62686ecb8688
Artun Akdogan <artun.akdogan@boun.edu.tr>
2022-05-03 20:39:39 +0300

    Complete students requirements except few points

--- src/students.py
+++ src/students.py
@@ -11,13 +11,19 @@ def homePage(req):
     try: action = int(action)
     except: action = 0
 
+    taken = run_statement(f"SELECT c.course_id, c.name, g.grade FROM Course c, Grades g, Students s WHERE \
+        s.username=\"{username}\" and s.student_id=g.student_id and c.course_id=g.course_id")
+
+    for course in parse(run_statement(f"SELECT added_courses FROM Students WHERE username=\"{username}\"")[0][0]):
+        taken = run_statement(f"Select course_id, name, null FROM Course WHERE course_id=\'{course}\'") + taken
     
     courses=run_statement(f"SELECT c.course_id, c.name, u.surname, i.department_id, c.credits, c.classroom_id, c.slot,\
         c.quota, c.prerequisites FROM Course c, Instructors i, User u WHERE c.instructor_username=i.username and i.username=u.username")
+
     # Remove square brackets and quotes
     courses = list(map(lambda i: i[:-1] + tuple([i[-1][1:-1].replace("\"", "")]), courses))
 
-    return render(req,'student.html',{"username":username, "action": action, "courses": courses})
+    return render(req,'student.html',{"username":username, "action": action, "courses": courses, "taken": taken})
 
 def addCourse(req):
     try:
@@ -33,16 +39,38 @@ def addCourse(req):
                     cnt+=1
             if cnt==len(prerequisites):
                 # Check quota and if course was added successfully before
-                if run_statement(f"Select * FROM Students as s, Course as c WHERE s.username=\"{username}\" and c.course_id=\"{course_id}\" and \
+                if run_statement(f"Select * FROM Students s, Course c WHERE s.username=\"{username}\" and c.course_id=\"{course_id}\" and \
                             not JSON_CONTAINS(s.added_courses, \'\"{course_id}\"\', \'$\') and \
                             (SELECT COUNT(*) FROM Students WHERE JSON_CONTAINS(added_courses, '\"{course_id}\"', '$')) < c.quota"):
                         
                     run_statement(f"UPDATE Students SET added_courses=JSON_ARRAY_APPEND(added_courses, '$', \'{course_id}\') \
-                        WHERE s.username=\"{username}\"")
+                        WHERE username=\"{username}\"")
                     return HttpResponseRedirect('../students?action=1')
     except:
         pass
     
     return HttpResponseRedirect('../students?action=2')
 
+def search(req):
+    keyword = req.POST["keyword"]    
+    data=run_statement(f"SELECT c.course_id, c.name, u.surname, i.department_id, c.credits, c.classroom_id, c.slot,\
+        c.quota, c.prerequisites FROM Course c, Instructors i, User u WHERE c.instructor_username=i.username and i.username=u.username\
+        and c.name REGEXP '{keyword}'")
+
+    # Remove square brackets and quotes
+    data = list(map(lambda i: i[:-1] + tuple([i[-1][1:-1].replace("\"", "")]), data))
+
     
+    return render(req,'table.html',{"title":f"Courses containing keyword {keyword}:", "headers": ("Course ID", "Course Name", "Instructor Surname", 
+        "Department", "Credits", "Classroom ID", "Time Slot", "Quota", "Prerequisites"), "data": data})
+
+def filter(req):
+    department_id = req.POST["department_id"]
+    campus = req.POST["campus"]
+    min_credit = req.POST["min_credit"]
+    max_credit = req.POST["max_credit"]
+
+    data = []
+
+    return render(req,'table.html',{"title":f"Courses filtered:", "headers": ("Course ID", "Course Name", "Instructor Surname", 
+        "Department", "Credits", "Classroom ID", "Time Slot", "Quota", "Prerequisites"), "data": data})
