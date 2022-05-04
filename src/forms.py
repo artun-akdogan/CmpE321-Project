fc4619244d58c0ed477d458fdce59d567a3211a7
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2022-05-04 20:32:47 +0300

    Comments added.

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-   # A form to login with the credentials.
+   # A form that accepts credentials and redirects user to login with that credentials.
 class UserLoginForm(forms.Form):
     username=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username'}))
     password=forms.CharField(widget=forms.PasswordInput)
