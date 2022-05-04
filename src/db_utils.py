a96df485327c097f1fc7051aa0389b158b1948d3
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2022-05-04 18:27:46 +0300

    comments added.

--- src/db_utils.py
+++ src/db_utils.py
@@ -2,13 +2,17 @@ from django.db import connection
 import hashlib
 import json
 
+# To execute a query and return the result of that query. 
+# We always needed to use this function. 
 def run_statement(statement):
     cursor= connection.cursor()
     cursor.execute(statement)
     return cursor.fetchall()
 
+# Hash the password as wanted, this code is executed.
 def hash(password):
     return hashlib.sha256(password.encode('UTF-8')).hexdigest()
 
+# this is shortcut to parse strings.
 def parse(string):
     return json.loads(string)
