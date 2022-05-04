9846d22ad821b342403fbb65348ad67c019f2170
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2022-05-04 19:33:25 +0300

    Update forms.py

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+   # A form to login with the credentials.
 class UserLoginForm(forms.Form):
     username=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username'}))
     password=forms.CharField(widget=forms.PasswordInput)
