3e2583028521f472740a5f0549051015593c6b65
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2022-05-04 19:04:34 +0300

    comments added.

--- src/students.py
+++ src/students.py
@@ -6,11 +6,12 @@ from .db_utils import run_statement, parse
 
 def homePage(req):
     
+    
     username=req.session["username"] #Retrieve the username of the logged-in user
     action = req.GET.get("action", 0) #Check the value of the GET parameter
     try: action = int(action)
     except: action = 0
-
+    
     taken = run_statement(f"SELECT c.course_id, c.name, g.grade FROM Course c, Grades g, Students s WHERE \
         s.username=\"{username}\" and s.student_id=g.student_id and c.course_id=g.course_id")
 
@@ -26,6 +27,7 @@ def homePage(req):
     return render(req,'student.html',{"username":username, "action": action, "courses": courses, "taken": taken})
 
 def addCourse(req):
+    # To add course.
     try:
         username = req.session["username"] #Retrieve the username of the logged-in user
         course_id = req.POST["course_id"].upper()
@@ -52,6 +54,7 @@ def addCourse(req):
     return HttpResponseRedirect('../students?action=2')
 
 def search(req):
+    # To search with the keyword(regex can be used here)
     keyword = req.POST["keyword"]    
     data=run_statement(f"SELECT c.course_id, c.name, u.surname, i.department_id, c.credits, c.classroom_id, c.slot,\
         c.quota, c.prerequisites FROM Course c, Instructors i, User u WHERE c.instructor_username=i.username and i.username=u.username\
@@ -64,13 +67,29 @@ def search(req):
     return render(req,'table.html',{"title":f"Courses containing keyword {keyword}:", "headers": ("Course ID", "Course Name", "Instructor Surname", 
         "Department", "Credits", "Classroom ID", "Time Slot", "Quota", "Prerequisites"), "data": data})
 
+'''
+PS: "filter" procedure
+CREATE PROCEDURE filter( IN department_id CHAR(100), 
+                        IN campus CHAR(100), 
+                        IN min_credit INT, 
+                        IN max_credit INT)
+BEGIN
+    SELECT c.course_id, c.name, u.surname, i.department_id, c.credits, c.classroom_id, c.slot,
+        c.quota, c.prerequisites
+    FROM Course c, User u, Instructors i, Classroom s
+    WHERE s.campus=campus and s.classroom_id=c.classroom_id and c.instructor_username=i.username
+        and u.username=i.username and i.department_id=department_id and min_credit <= c.credits and c.credits <= max_credit;
+END;
+'''
+
 def filter(req):
+    # To filter the results, firstly we need to collect the parameters.
     department_id = req.POST["department_id"]
     campus = req.POST["campus"]
     min_credit = req.POST["min_credit"]
     max_credit = req.POST["max_credit"]
-
+    # Then call the filter function with the parameters.
     data = run_statement(f"CALL filter(\'{department_id}\',\'{campus}\',{min_credit},{max_credit})")
-
+    
     return render(req,'table.html',{"title":f"Courses filtered:", "headers": ("Course ID", "Course Name", "Instructor Surname", 
         "Department", "Credits", "Classroom ID", "Time Slot", "Quota", "Prerequisites"), "data": data})
